9c2cf944c05f8c4efe449b575a971ff63f4bf1d4
= <=>
2017-04-26 00:01:15 +0300

    Add auth layer

---
+++ routes.php
@@ -0,0 +1,19 @@
+<?php
+
+use Symfony\Component\Finder\Finder;
+
+Route::get('/', function(){
+//    $files = Finder::create()->in(app_path())
+//                             ->name('*, txt'))
+//                               ->contains('{example-key}');
+//    foreach ($files as $file) {
+//         $contents = File::get($file->getRealPath());
+//         $updated = str_replace('{example-key}', 'UPDATED!', $contents);
+//         File::put($file->getRealPath(), $updated);
+      return view ('welcome');
+
+});
+
+Route::get('about', function(){
+
+});
